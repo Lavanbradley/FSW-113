7ae24b932be873b140f442fd39a0e79568543d68
Lavanbradley <lavan.bradley@bryanuniversity.edu>
2021-07-25 14:48:32 -0700

    week 6

--- week6/Assinment-1/js/index.js
+++ week6/Assinment-1/js/index.js
@@ -1,10 +1,10 @@
-import { displayResults } from "./data.js"
-import data from "./getData.js";
-import { getData } from "./getData.js"
 /* 
   index.js
 */
-
+var h1 = document.getElementById('h1')
+h1.style.color = "Red"
+var spinner = document.querySelector('.loading')
+spinner.style.visibility = "hidden"
 $(document).ready(function () {
 
   "use strict";
@@ -23,29 +23,39 @@ $(document).ready(function () {
   listItems.css("font-weight", "bold");
   listItems.filter(":first").css("font-size", "18px");
 
-  $("#starWarApiSearchForm").on("submit", function (e) {
-    // debugger
-    // var peopleId = $("#peopleId").val();
-    e.preventDefault()
+  $("#starWarApiSearchForm").on("submit", function () {
+    debugger
+    var peopleId = $("#peopleId").val();
+
     if (peopleId) {
 
       resultList.text("Performing search...");
+      
+      spinner.style.visibility = "visible"
 
-      getData()
-      console.log(data, data.length)
-      var newData = data
-      console.log(newData.length, data.length)
-       displayResults(data)
+      var starWarApiSearch = "https://swapi.dev/api/people/" + peopleId;
+
+      $.get(starWarApiSearch)
+        .success(function (r) {
+          displayResults(r);
+        })
+        .fail(function (err) {
+          console.log("Failed to query Star war API");
+        })
+        .done(function () {
+          //
+        });
     }
 
     return false;
   });
 
-  // function displayResults(results) {
-  //  console.log(results)
-  //   resultList.text(JSON.stringify(results));
+  function displayResults(results) {
 
+    resultList.text(JSON.stringify("Name: " + results.name + " Height: " + results.height + " Films: " + results.films));
+    
+    spinner.style.visibility = "hidden"
 
-  // }
+  }
 
 });
